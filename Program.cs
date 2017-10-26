84882c433399dcdded47ff420aeaa7e61306e20a
jamiefutch <jamiefutch@gmail.com>
2017-10-26 12:39:03 -0400

    initial check in
    initial check in

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace NewGUID
 	{
 		static void Main(string[] args)
 		{
+			string g = Guid.NewGuid().ToString();
+			Console.WriteLine("===============================================================================");
+			Console.WriteLine("GUID: ");
+			Console.WriteLine("===============================================================================");
+			Console.WriteLine("");
+			Console.WriteLine(g);
+			Console.WriteLine("");
+			Console.WriteLine("Press Any Key To Continue");
+			Console.ReadKey();
 		}
 	}
 }
